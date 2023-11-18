508ba6be20435618a4c1f182b88339af55c11698
alberbendayan <108437189+alberbendayan@users.noreply.github.com>
2023-11-18 18:19:41 -0300

    search tableta

--- app/src/main/java/ar/edu/itba/grupo10/vfit/ui/screens/Search.kt
+++ app/src/main/java/ar/edu/itba/grupo10/vfit/ui/screens/Search.kt
@@ -33,47 +33,81 @@ import androidx.compose.material3.OutlinedButton
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import ar.edu.itba.grupo10.vfit.ui.main.WindowInfo
+import ar.edu.itba.grupo10.vfit.ui.main.rememberWindowInfo
 import ar.edu.itba.grupo10.vfit.ui.theme.VFitTheme
 
 //@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SearchScreen(
-
 ) {
+
+    val windowSize = rememberWindowInfo()
     Surface {
         Column(
             modifier = Modifier
                 .fillMaxSize(1f)
-                .verticalScroll(rememberScrollState())
-        ) {
+                .verticalScroll(rememberScrollState()),
+            horizontalAlignment = Alignment.CenterHorizontally
+            ) {
             var search by rememberSaveable { mutableStateOf("") }
 
-            Row(
-                modifier = Modifier.padding(top = 10.dp, bottom = 10.dp)
-            ){
+           Column(
+               horizontalAlignment = Alignment.CenterHorizontally,
+           ) {
 
-                TextField(
-                    value = search,
-                    onValueChange = { search = it },
-                    label = { Text(stringResource(R.string.search)) },
-                    singleLine = true,
-                    modifier = Modifier.clip(shape = RoundedCornerShape(size = 25.dp)),
-                    trailingIcon = {
-                        IconButton(onClick = { /*TODO: search*/ }) {
-                            Icon(imageVector =Icons.Filled.Search , contentDescription = "Search")
-                        }
+
+                Row(
+
+                    modifier = Modifier.padding(top = 10.dp, bottom = 10.dp)
+                ){
+
+                    if(windowSize.screenWidthInfo == WindowInfo.WindowType.Expanded){
+                        TextField(
+                            value = search,
+                            onValueChange = { search = it },
+                            label = { Text(stringResource(R.string.search)) },
+                            singleLine = true,
+                            modifier = Modifier.clip(shape = RoundedCornerShape(size = 25.dp))
+                                .width(500.dp),
+                            trailingIcon = {
+                                IconButton(onClick = { /*TODO: search*/ }) {
+                                    Icon(
+                                        imageVector = Icons.Filled.Search,
+                                        contentDescription = "Search"
+                                    )
+                                }
+                            }
+                        )
+                    }else{
+
+                            TextField(
+                            value = search,
+                            onValueChange = { search = it },
+                            label = { Text(stringResource(R.string.search)) },
+                            singleLine = true,
+                            modifier = Modifier.clip(shape = RoundedCornerShape(size = 25.dp)),
+                            trailingIcon = {
+                                IconButton(onClick = { /*TODO: search*/ }) {
+                                    Icon(
+                                        imageVector = Icons.Filled.Search,
+                                        contentDescription = "Search"
+                                    )
+                                }
+                            }
+                        )
                     }
-                )
 
-                OutlinedButton(
-                    modifier = Modifier.width(160.dp),
-                    onClick = { search="" }
-                ) {
-                    Text(stringResource(R.string.cancel))
+                    OutlinedButton(
+                        modifier = Modifier.width(160.dp),
+                        onClick = { search="" }
+                    ) {
+                        Text(stringResource(R.string.cancel))
+                    }
+
+
                 }
-
-
-            }
+           }
 
             Column (
                 horizontalAlignment = Alignment.CenterHorizontally,
@@ -82,18 +116,29 @@ fun SearchScreen(
                 Text(
                     text = stringResource(R.string.look_for_results),
                     fontSize = 40.sp,
-                    modifier = Modifier.padding(top = 80.dp)
+                    modifier = Modifier.padding(top = 40.dp)
                 )
-
+                if(windowSize.screenWidthInfo == WindowInfo.WindowType.Expanded){
                 Image(
                     modifier = Modifier
-                        .width(400.dp)
+                        .width(700.dp)
                         .padding(top = 25.dp)
                         .clip(shape = RoundedCornerShape(size = 12.dp)),
                     painter = painterResource(id = R.drawable.background_search),
                     contentDescription = null,
                     contentScale = ContentScale.Crop,
-                )
+                )}
+                else{
+                    Image(
+                        modifier = Modifier
+                            .width(400.dp)
+                            .padding(top = 25.dp)
+                            .clip(shape = RoundedCornerShape(size = 12.dp)),
+                        painter = painterResource(id = R.drawable.background_search),
+                        contentDescription = null,
+                        contentScale = ContentScale.Crop,
+                    )
+                }
             }
 
         }
@@ -101,10 +146,18 @@ fun SearchScreen(
 }
 
 
-
-@Preview(showSystemUi = true, locale = "es")
+@Preview(showSystemUi = true, locale = "es", device = "spec:width=411dp,height=891dp")
 @Composable
 fun SearchScreenPreview() {
+    VFitTheme {
+        SearchScreen()
+    }
+}
+
+
+@Preview(showSystemUi = true, locale = "es", device = "spec:width=1280dp,height=800dp,dpi=240")
+@Composable
+fun SearchScreenPreview1() {
     VFitTheme {
         SearchScreen()
     }
